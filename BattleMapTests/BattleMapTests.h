8efc078e1a8823f51c8e266367459bb5b80bd77c
al6ert <albertperezrabella@hotmail.com>
2012-05-16 20:35:18 +0300

    Update BattleMapTests/BattleMapTests.h

--- BattleMapTests/BattleMapTests.h
+++ BattleMapTests/BattleMapTests.h
@@ -5,7 +5,7 @@
 //  Created by Albert Perez Rabella on 12/05/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-//  Test github 2
+//  Test github 3
 
 #import <SenTestingKit/SenTestingKit.h>
 
